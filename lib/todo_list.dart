15708b336ec942750caa369a45736c4cd43d16c1
unknown <inanakmugan@gmail.com>
2022-06-02 12:44:21 +0300

    alternative todo card

--- lib/todo_list.dart
+++ lib/todo_list.dart
@@ -51,12 +51,62 @@ class _ToDoListState extends State<ToDoList> {
   }
 
   Widget _itembuild(BuildContext context, int index) {
-    return CheckboxListTile(
-      title: Text(todos[index].title),
-      value: todos[index].isChanged,
-      onChanged: (bool? value) {
-        toggleTodo(todos[index], value as bool);
-      },
+    //   return CheckboxListTile(
+    //     title: Text(todos[index].title),
+    //     value: todos[index].isChanged,
+    //     onChanged: (bool? value) {
+    //       toggleTodo(todos[index], value as bool);
+    //     },
+    //   );
+    // }
+
+    return Container(
+      padding: EdgeInsets.only(left: 15, right: 10),
+      height: 100,
+      margin: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(
+          Radius.circular(10),
+        ),
+        gradient: LinearGradient(
+          colors: [Colors.yellow[700] as Color, Colors.redAccent],
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight,
+          tileMode: TileMode.clamp,
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: Offset(0, 3), // changes position of shadow
+          ),
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Text(
+            'Yapılacak',
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white70,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Spacer(),
+          Checkbox(
+            value: todos[index].isChanged,
+            onChanged: (bool? value) {
+              toggleTodo(todos[index], value as bool);
+            },
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.delete),
+          )
+        ],
+      ),
     );
   }
 
